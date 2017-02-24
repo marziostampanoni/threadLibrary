799facf034fecae1ea9dc729af4cd8c1e447bb6e
n0skill <n0skill@n0skills-mbp.supsi.ch>
2017-02-24 15:30:43 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/threadLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/threadLibrary.dir/main.c.o"
+  "threadLibrary.pdb"
+  "threadLibrary"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/threadLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
