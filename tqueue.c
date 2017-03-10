88dd213a9c0117433093f5430595e2bd54c96294
niko <niko.bonomi@gmail.com>
2017-03-10 15:46:47 +0100

    Added tqueue_enqueue method

--- tqueue.c
+++ tqueue.c
@@ -2,4 +2,31 @@
 // Created by Elia Oggian on 10.03.17.
 //
 
+#include <stdlib.h>
 #include "tqueue.h"
+
+unsigned long int tqueue_enqueue(TQueue* q, void* data){
+
+    if((*q)== NULL){
+        *q = (TQueueNode*)malloc(sizeof(TQueueNode*));
+        (*q)->next = *q;
+        (*q)->data = data;
+        return 1;
+    }
+
+
+    unsigned long int position = 0;
+    TQueue* qStart = q;
+
+
+    while((*q)->next != *qStart){
+        *q = (*q)->next;
+        position++;
+    }
+    (*q)->next = (TQueueNode*)malloc(sizeof(TQueueNode*));
+    (*q)->next->data = data;
+    (*q)->next->next = *qStart;
+
+
+    return position;
+}
