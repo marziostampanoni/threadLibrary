0d50a6e51150661d8ad0108be61da2606463dc3b
eliaoggian <etuz93@gmail.com>
2017-03-10 15:42:27 +0100

    implemented size
    Signed-off-by: eliaoggian <etuz93@gmail.com>

--- tqueue.c
+++ tqueue.c
@@ -2,4 +2,18 @@
 // Created by Elia Oggian on 10.03.17.
 //
 
+#include <stddef.h>
 #include "tqueue.h"
+
+unsigned long int tqueue_size(TQueue q){
+
+    TQueue initial = q;
+
+    unsigned long int size=1;
+
+    while (q->next != initial){
+        q = q->next;
+        size++;
+    }
+    return size;
+}
