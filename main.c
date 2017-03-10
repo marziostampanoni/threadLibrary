c25bf2eb88257e0d86e05c5a3802fc36f66ffa93
Adriatik Dushica <adriatik.dushica@gmail.com>
2017-03-10 15:36:07 +0100

    aggiunta funzionalità tqueue_at_offset

--- main.c
+++ main.c
@@ -4,5 +4,8 @@
 
 int main() {
     printf("Hello, World!\n");
+
+
+
     return 0;
 }
--- tqueue.c
+++ tqueue.c
@@ -2,4 +2,14 @@
 // Created by Elia Oggian on 10.03.17.
 //
 
+#include <stdlib.h>
 #include "tqueue.h"
+
+TQueue tqueue_at_offset(TQueue q, unsigned long int offset) {
+    TQueueNode* result = NULL;
+
+    for (int i = 0; i < offset % tqueue_size(q); ++i)
+        result = q->next;
+
+    return result;
+}
