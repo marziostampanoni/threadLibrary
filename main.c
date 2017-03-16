5750ccb9d7cca32611cb24966072f89c5e3ac4e8
Galgiani Matteo <mgalgiani@gmail.com>
2017-03-16 19:13:33 +0100

    pushing branch pop segmentation fault!?

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "tqueue.h"
 
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Queue Test!\n");
+
+
+    TQueue first = malloc(sizeof(TQueue));
+    TQueue second = malloc(sizeof(TQueue));
+
+    first->next = second;
+    second->next = first;
+    printf("The size of the given queue is %lu\n", tqueue_size(first));
+
+    TQueue *queue = &first;
+    printf("%p", tqueue_pop(queue));
+
+    TQueueNode *temp = first;
+
+    printf("The size of the given queue is %lu\n", tqueue_size(*queue));
+
+    printf("%p = %p\n", first, second);
+    assert(temp == first);
+    assert(tqueue_size(temp) == 1);
+    assert(*queue == second);
+
     return 0;
 }
--- tqueue.c
+++ tqueue.c
@@ -2,18 +2,80 @@
 // Created by Elia Oggian on 10.03.17.
 //
 
+#include <stdlib.h>
+#include <stddef.h>
 #include "tqueue.h"
+
+unsigned long int tqueue_enqueue(TQueue* q, void* data){
+
+    if((*q)== NULL){
+        *q = (TQueueNode*)malloc(sizeof(TQueueNode*));
+        (*q)->next = *q;
+        (*q)->data = data;
+        return 1;
+    }
+
+
+    unsigned long int position = 0;
+    TQueue* qStart = q;
+
+
+    while((*q)->next != *qStart){
+        *q = (*q)->next;
+        position++;
+    }
+    (*q)->next = (TQueueNode*)malloc(sizeof(TQueueNode*));
+    (*q)->next->data = data;
+    (*q)->next->next = *qStart;
+
+
+    return position;
+}
 
 /* Removes and returns the element at the beginning of the list, NULL if the
 queue is empty */
 void* tqueue_pop(TQueue* q){
-    if(tqueue_size(q) == 0)
+    unsigned long size = tqueue_size((*q));
+    if(size == 0)
         return NULL;
-    TQueueNode* temp = q;
-    if(q != q->next)
-        q = q->next;
     else{
-        q = NULL;
+        TQueue temp = *q;
+        temp->next = NULL;
+
+        if(size == 1){
+            *q = NULL;
+        }else {
+            TQueueNode *prev = *q;
+
+            while (prev->next != *q) {
+                prev = prev->next;
+            }
+
+            prev->next = (*q)->next;
+            *q = (*q)->next;
+        }
+        return temp;
     }
-    return temp;
+}
+
+unsigned long int tqueue_size(TQueue q){
+
+    TQueue initial = q;
+
+    unsigned long int size=1;
+
+    while (q->next != initial){
+        q = q->next;
+        size++;
+    }
+    return size;
+}
+
+TQueue tqueue_at_offset(TQueue q, unsigned long int offset) {
+    TQueueNode* result = NULL;
+
+    for (int i = 0; i < offset % tqueue_size(q); ++i)
+        result = q->next;
+
+    return result;
 }
