6ebadda122abe59b3e74644b98c0a041a9439f4b
niko <niko.bonomi@gmail.com>
2017-03-24 15:46:34 +0100

    Fixed bthread e bthread_private copy and paste

--- bthread.h
+++ bthread.h
@@ -1,31 +1,16 @@
 //
 // Created by niko on 24.03.17.
 //
-
-#include "tqueue.h"
+//
 #include "setjmp.h"
 #include "bthread_private.h"
 
 #ifndef THREADLIBRARY_BTHREAD_H
 #define THREADLIBRARY_BTHREAD_H
 
-#endif //THREADLIBRARY_BTHREAD_H
-
-
-typedef struct {
-    TQueue queue;
-    TQueue current_item;
-    jmp_buf context;
-} __bthread_scheduler_private;
-
-typedef unsigned long int bthread_t;
-
-typedef enum { __BTHREAD_EXITED = 0, __BTHREAD_ZOMBIE, __BTHREAD_UNINITIALIZED,
-    __BTHREAD_READY, __BTHREAD_BLOCKED, __BTHREAD_SLEEPING } bthread_state;
-
 
 int bthread_create(bthread_t *bthread, const bthread_attr_t *attr,
-                   void *(*start_routine) (void *), void *arg);
+                   void *(*start_routine)(void *), void *arg);
 
 int bthread_join(bthread_t bthread, void **retval);
 
@@ -33,3 +18,5 @@ void bthread_yield();
 
 void bthread_exit(void *retval);
 
+
+#endif //THREADLIBRARY_BTHREAD_H
--- bthread_private.h
+++ bthread_private.h
@@ -1,8 +1,40 @@
 //
 // Created by niko on 24.03.17.
 //
-
 #ifndef THREADLIBRARY_BTHREAD_PRIVATE_H_H
 #define THREADLIBRARY_BTHREAD_PRIVATE_H_H
+
+#include <setjmp.h>
+#include "tqueue.h"
+
+typedef unsigned long int bthread_t;
+
+typedef struct {
+
+} bthread_attr_t;
+
+typedef enum {
+    __BTHREAD_EXITED = 0, __BTHREAD_ZOMBIE, __BTHREAD_UNINITIALIZED,
+    __BTHREAD_READY, __BTHREAD_BLOCKED, __BTHREAD_SLEEPING
+} bthread_state;
+
+typedef void *(*bthread_routine)(void *);
+
+typedef struct {
+    bthread_t tid;
+    bthread_routine body;
+    void *arg;
+    bthread_state state;
+    bthread_attr_t attr;
+    jmp_buf context;
+    void *retval;
+}__bthread_private;
+
+
+typedef struct {
+    TQueue queue;
+    TQueue current_item;
+    jmp_buf context;
+} __bthread_scheduler_private;
 
 #endif //THREADLIBRARY_BTHREAD_PRIVATE_H_H
