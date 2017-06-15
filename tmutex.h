108bf792f8f6ff2398ab323c8fffc019448a503e
niko <niko.bonomi@gmail.com>
2017-06-15 15:36:56 +0200

    mutex added

---
+++ tmutex.h
@@ -0,0 +1,30 @@
+//
+// Created by niko on 15.06.17.
+//
+#ifndef __TMUTEX_H__
+#define __TMUTEX_H__
+
+#include "tqueue.h"
+
+typedef struct {
+
+    void* owner;
+
+    TQueue waiting_list;
+} bthread_mutex_t;
+
+// Defined only for "compatibility" with pthread
+typedef struct {} bthread_mutexattr_t;
+
+// attr is ignored
+int bthread_mutex_init(bthread_mutex_t* m, const bthread_mutexattr_t *attr);
+
+int bthread_mutex_destroy(bthread_mutex_t* m);
+
+int bthread_mutex_lock(bthread_mutex_t* m);
+
+int bthread_mutex_trylock(bthread_mutex_t* m);
+
+int bthread_mutex_unlock(bthread_mutex_t* m);
+
+#endif
